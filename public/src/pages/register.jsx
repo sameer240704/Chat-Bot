52e072421d33f76070ceb6d2f38146acf3745df0
sameer240704 <sameergupta79711@gmail.com>
2024-01-21 02:39:44 +0530

    Added register css

--- public/src/pages/register.jsx
+++ public/src/pages/register.jsx
@@ -55,7 +55,14 @@ export default function Register() {
               placeholder="Confirm Password"
               onChange={(event) => {handleChange(event)}}
             />
-            <button type="submit">Sign Up</button>
+            <button type="submit">
+              <span class="circle1"></span>
+              <span class="circle2"></span>
+              <span class="circle3"></span>
+              <span class="circle4"></span>
+              <span class="circle5"></span>
+              <span class="text">Sign Up</span>
+            </button>
             <span>Already have an account? <Link to="/login">Sign In Now</Link></span>
         </form>
       </FormContainer>
@@ -75,14 +82,17 @@ const FormContainer = styled.div`
   justify-content: center;
   align-items: center;
   color: #ffffff;
+  
   .logo {
     height: 50px;
     width: 50px;
   }
+
   .logo:hover {
     transform: rotateY(45deg);
     animation: rotateAnimation 1s linear infinite;
   }
+
   form {
     height: 600px;
     width: 400px;
@@ -91,8 +101,9 @@ const FormContainer = styled.div`
     display: flex;
     flex-direction: column;
     border: 0;
-    border-radius: px;
+    border-radius: 10px;
   }
+
   .header {
     display: flex;
     flex-direction: column;
@@ -107,6 +118,7 @@ const FormContainer = styled.div`
       margin-bottom: 20px;
     }
   }
+
   input {
     border: 0;
     border-radius: 5px;
@@ -116,15 +128,63 @@ const FormContainer = styled.div`
     color: #fff7f7;
     font-size: 16px;
   }
+
   button {
+    font-weight: bold;
+    color: white;
+    background-color: #171717;
+    padding: 1em 2em;
+    border: none;
+    border-radius: .6rem;
     margin-top: 20px;
     padding: 15px;
-    border: 0;
-    border-radius: 5px;
     font-size: 16px;
     margin-bottom: 10px;
+    position: relative;
+    cursor: pointer;
+    overflow: hidden;
   }
-
+  
+  button span:not(:nth-child(6)) {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    height: 76px;
+    width: 76px;
+    background-color: #0c66ed;
+    border-radius: 50%;
+    transition: .6s ease;
+  }
+  
+  button span:nth-child(6) {
+    position: relative;
+  }
+  
+  button span:nth-child(1) {
+    transform: translate(-7em, -8em);
+  }
+  
+  button span:nth-child(2) {
+    transform: translate(-5em, 1.7em);
+  }
+  
+  button span:nth-child(3) {
+    transform: translate(-.2em, 1.8em);
+  }
+  
+  button span:nth-child(4) {
+    transform: translate(3.5em, 1.4em);
+  }
+  
+  button span:nth-child(5) {
+    transform: translate(3.5em, -3.8em);
+  }
+  
+  button:hover span:not(:nth-child(6)) {
+    transform: translate(-50%, -50%) scale(4);
+    transition: 1.5s ease;
+  }  
 
   @keyframes rotateAnimation {
 	  from { 
